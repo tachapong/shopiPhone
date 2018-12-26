75d5693111087872cd648907f3ad61c6be0c8d7c
tatae <tachapong999.gmail.com>
2018-12-26 11:52:57 +0700

    add swagger

---
+++ shopapi/Controllers/OrderController.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace shopapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+       
+    }
+}
---
+++ shopapi/Models/Order.cs
@@ -0,0 +1,13 @@
+namespace shopapi.Models
+{
+    public class Order
+    {
+     public string Id { get; set; }  
+     public string Name { get; set; } 
+     public int Price { get; set; }
+     public int Amount { get; set; }
+      public string Group { get; set; }
+        
+    }
+
+}
