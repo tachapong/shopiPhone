2f13d9115360c53a23263716169c9f3a58d7c478
tatae <tachapong999.gmail.com>
2018-12-26 11:35:14 +0700

    create project api, ionic

---
+++ shopionic/src/pages/home/home.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController, public http: HttpClient) {
+
+  }
+
+}
